d009b6399e70efa5c08627f496160dbb4060b82c
H. Cole Wiley <cole@scandy.co>
2018-11-20 13:51:39 -0600

    using the Scandy fork for Poisson

--- Remote/PoissonReconstruction.remote.cmake
+++ Remote/PoissonReconstruction.remote.cmake
@@ -4,7 +4,7 @@
 
 vtk_fetch_module(PoissonReconstruction
   "Poisson Surface reconstruction from unorganized points"
-  GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
+  GIT_REPOSITORY https://github.com/Scandy-co/PoissonReconstruction
   # September 7, 2017 - refactored remote module
   GIT_TAG ffaa97c271b8dfb91d7742dd3c05d7377d864afe
   )
